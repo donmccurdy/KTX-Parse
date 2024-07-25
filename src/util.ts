7b3d6fc6339c95ed3450f8443b2ae9ed2d374087
yue <github@yue.coffee>
2024-07-25 09:21:29 +0900

    fix: remove unnecessary Buffer.from usage (#148)

--- src/util.ts
+++ src/util.ts
@@ -47,18 +47,12 @@ import type { KTX2Container } from './container.js';
 
 /** Encodes text to an ArrayBuffer. */
 export function encodeText(text: string): Uint8Array {
-	if (typeof TextEncoder !== 'undefined') {
-		return new TextEncoder().encode(text);
-	}
-	return Buffer.from(text);
+	return new TextEncoder().encode(text);
 }
 
 /** Decodes an ArrayBuffer to text. */
 export function decodeText(buffer: Uint8Array): string {
-	if (typeof TextDecoder !== 'undefined') {
-		return new TextDecoder().decode(buffer);
-	}
-	return Buffer.from(buffer).toString('utf8');
+	return new TextDecoder().decode(buffer);
 }
 
 /** Concatenates N ArrayBuffers. */
