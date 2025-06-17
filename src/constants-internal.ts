57e728604c609f83b0723279d044c48ce11bd296
Don McCurdy <dm@donmccurdy.com>
2025-06-17 18:11:07 -0400

    chore(build): Modernize tsconfig.json (#191)

--- src/constants-internal.ts
+++ src/constants-internal.ts
@@ -5,9 +5,9 @@
 // Injected at compile time, from $npm_package_version.
 declare const PACKAGE_VERSION: string;
 
-export const KTX_WRITER = `KTX-Parse v${PACKAGE_VERSION}`;
+export const KTX_WRITER: string = `KTX-Parse v${PACKAGE_VERSION}`;
 
-export const NUL = new Uint8Array([0x00]);
+export const NUL: Uint8Array = new Uint8Array([0x00]);
 
 export type vec3 = [number, number, number];
 export type vec2 = [number, number, number];
@@ -16,7 +16,7 @@ export type vec2 = [number, number, number];
 // KTX2 Header.
 ///////////////////////////////////////////////////
 
-export const KTX2_ID = [
+export const KTX2_ID: number[] = [
 	// '´', 'K', 'T', 'X', '2', '0', 'ª', '\r', '\n', '\x1A', '\n'
 	0xab, 0x4b, 0x54, 0x58, 0x20, 0x32, 0x30, 0xbb, 0x0d, 0x0a, 0x1a, 0x0a,
 ];
